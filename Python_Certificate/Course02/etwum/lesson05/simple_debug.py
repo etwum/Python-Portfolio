e28aa8d0f422696bf74c11493a8b2376e8eea961
etwum <33198751+etwum@users.noreply.github.com>
2018-08-02 17:42:17 -0700

    adding files

---
+++ Python_Certificate/Course02/etwum/lesson05/simple_debug.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  8 16:02:53 2018
+
+@author: Emmanuel
+"""
+
+# simple.py
+def my_fun():
+    for i in range(1, 500):
+         123/ (50 - i)
+
+if __name__ == '__main__':
+    my_fun()
